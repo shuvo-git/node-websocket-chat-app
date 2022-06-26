b7d97dfca03d7a832fa0e561aa00981a4d26511c
shuvo-git <shuvo.pma@gmail.com>
2022-06-26 17:50:17 +0600

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const { WebSocket } = require('ws');
+
+const express = require('express');
+const app = express();
+
+const port = 6969;
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', function connection(ws){
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if(client != ws && client.readyState == WebSocket.OPEN){
+                client.send(data);
+            }
+        })
+    })
+});
+
+server.listen(port, function () {
+    console.log(`server is listening on port ${port}`);
+});
